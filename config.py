76d21185d76a6a77877be93eb7cf8bf6a27fc771
Mateusz Koniuszewski <mateusz.koniuszewski@gmail.com>
2022-08-25 16:25:02 +0200

    Added workaround for postgres driver name change from postrgres to postgresql

--- config.py
+++ config.py
@@ -2,9 +2,17 @@ import datetime
 import os
 
 
+def _translate_postgres_driver(database_url: str):
+    if 'postgres' in database_url and 'postgresql' not in database_url:
+        return database_url.replace("postgres", "postgresql")
+    return database_url
+
+
 class Config:
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = _translate_postgres_driver(
+        os.environ.get("DATABASE_URL")
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     SECRET_KEY = os.environ["APP_SECRET_KEY"]

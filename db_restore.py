92e5c3f6383e9816dc3660d1ade4f718b3d49393
Mateusz Koniuszewski <mateusz.koniuszewski@gmail.com>
2022-11-19 12:25:37 +0100

    Function for restoring database

--- db_restore.py
+++ db_restore.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+import logging
 import subprocess
 
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-
-from config import logger
 
 
 def restore_postgres_db(db_host, db, port, user, password, backup_file, verbose):
@@ -93,78 +92,63 @@ def swap_restore_active(db_host, restore_database, active_database, db_port, use
         exit(1)
 
 
-def main():
+def main(
+        postgres_host,
+        postgres_port,
+        postgres_db,
+        postgres_user,
+        postgres_password,
+        restore_file,
+):
+    postgres_restore = "{}_restore".format(postgres_db),
+
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    args_parser = argparse.ArgumentParser(description='Postgres database management')
-    args_parser.add_argument("--action",
-                             metavar="action",
-                             choices=['list', 'list_dbs', 'restore', 'backup'],
-                             required=True)
-    args_parser.add_argument("--date",
-                             metavar="YYYY-MM-dd",
-                             help="Date to use for restore (show with --action list)")
-    args_parser.add_argument("--dest-db",
-                             metavar="dest_db",
-                             default=None,
-                             help="Name of the new restored database")
-    args_parser.add_argument("--verbose",
-                             default=True,
-                             help="verbose output")
-    args_parser.add_argument("--configfile",
-                             required=True,
-                             help="Database configuration file")
-    args = args_parser.parse_args()
 
-    config = configparser.ConfigParser()
-    config.read(args.configfile)
 
-    postgres_host = 'postgres-a5eek5'
-    postgres_port = '5432'
-    postgres_db = 'notesdb'
-    postgres_restore = "{}_restore".format(postgres_db)
-    postgres_user = 'posgres-user'
-    postgres_password = '4qDIdXwn63ybGzvQhfK5zTbBlhRgFS'
-    restore_filename = '/tmp/restore.dump.gz'
-    restore_uncompressed = '/tmp/restore.dump'
+    # logger.info("Creating temp database for restore : {}".format(postgres_restore))
+    # tmp_database = create_db(postgres_host,
+    #           postgres_restore,
+    #           postgres_port,
+    #           postgres_user,
+    #           postgres_password)
+    # logger.info("Created temp database for restore : {}".format(tmp_database))
+    # logger.info("Restore starting")
+    result = restore_postgres_db(postgres_host,
+                                 postgres_db,
+                                 postgres_port,
+                                 postgres_user,
+                                 postgres_password,
+                                 restore_file,
+                                 True)
+    for line in result.splitlines():
+        logger.info(line)
+    logger.info("Restore complete")
 
-    if args.action == "restore":
-        logger.info("Creating temp database for restore : {}".format(postgres_restore))
-        tmp_database = create_db(postgres_host,
-                  postgres_restore,
-                  postgres_port,
-                  postgres_user,
-                  postgres_password)
-        logger.info("Created temp database for restore : {}".format(tmp_database))
-        logger.info("Restore starting")
-        result = restore_postgres_db(postgres_host,
-                                     postgres_restore,
-                                     postgres_port,
-                                     postgres_user,
-                                     postgres_password,
-                                     restore_uncompressed,
-                                     args.verbose)
-        for line in result.splitlines():
-            logger.info(line)
-        logger.info("Restore complete")
+    # restored_db_name = postgres_db
+    # logger.info("Switching restored database with active one : {} > {}".format(
+    #     postgres_restore, restored_db_name
+    # ))
+    # swap_restore_active(postgres_host,
+    #                     postgres_restore,
+    #                     restored_db_name,
+    #                     postgres_port,
+    #                     postgres_user,
+    #                     postgres_password)
 
-        restored_db_name = postgres_db
-        logger.info("Switching restored database with active one : {} > {}".format(
-            postgres_restore, restored_db_name
-        ))
-        swap_restore_active(postgres_host,
-                            postgres_restore,
-                            restored_db_name,
-                            postgres_port,
-                            postgres_user,
-                            postgres_password)
-
-        logger.info("Database restored and active.")
+    logger.info("Database restored and active.")
 
 
 if __name__ == '__main__':
-    main()
+    main(
+        postgres_host='',
+        postgres_port='',
+        postgres_db='',
+        postgres_user='',
+        postgres_password='',
+        restore_file='',
+    )

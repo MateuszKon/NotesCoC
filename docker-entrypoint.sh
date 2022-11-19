aa7906e0d11337e07053edd42c40e2de0e9eda2e
MateuszKon <mateusz.koniuszewski@gmail.com>
2022-11-19 11:03:14 +0100

    WIP: https enabling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,11 @@ echo "DATABASE CONNECTION IS UP"
 
 flask db upgrade
 
+cd /etc/nginx/
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout localhost.key -out localhost.crt
+
 service nginx restart
 cat /var/log/nginx/error.log
 
+cd /app
 gunicorn -b 127.0.0.1:8080 --workers 4 --preload app:app
